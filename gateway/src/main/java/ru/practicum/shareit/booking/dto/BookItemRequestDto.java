e17d92f45e182f5db354f282faf16a40e685bd87
Elena <zaryanskaya@yandex.ru>
2023-07-10 03:02:13 +0000

    Спринт 16

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,9 @@ import lombok.NoArgsConstructor;
 public class BookItemRequestDto {
     private long itemId;
     @FutureOrPresent
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime start;
     @Future
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime end;
 }
--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoForItem.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoForItem.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,9 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class BookingDtoForItem {
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime start;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime end;
     private Long bookerId;
     private BookingState status;
